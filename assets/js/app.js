dda26f516ffc987449adc4224d4c09e32636a613
CircuitSyn <circuitsyn@gmail.com>
2018-12-10 22:35:45 -0800

    show

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // Message Submission Thank you Modal Trigger
 $(document).ready(function() {
 
-    if(window.location.href.indexOf('#thankYou') == -1) {
-      $('#thankYou').modal('toggle');
+    if(window.location.href.indexOf('#thankYou') != -1) {
+      $('#thankYou').modal('show');
     }
   
   });
