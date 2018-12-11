cef421590a2025838323854e83d6cde8858de4de
CircuitSyn <circuitsyn@gmail.com>
2018-12-10 21:54:12 -0800

    testing modal

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Message Submission Thank you Modal Trigger
+$(document).ready(function() {
+
+    if(window.location.href.indexOf('#thankYou') != -1) {
+      $('#thankYou').modal('show');
+    }
+  
+  });
