17c3955c58cb3748f2721e9cb2c580388f6a893b
CircuitSyn <circuitsyn@gmail.com>
2018-12-13 17:37:44 -0800

    added scroll magic

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,32 @@ $(document).ready(function() {
 )
 
 
+    //------------ Scroll Magic
+	$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("section.panel");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i])
+				// .addIndicators()
+				.addTo(controller);
+		}
+	});
+
+
+  //end of document on ready section
   });
+
+
+  
