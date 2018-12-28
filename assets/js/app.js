b64294a336df0aa9a664b83474ee90746f2ec4e7
CircuitSyn <circuitsyn@gmail.com>
2018-12-28 11:27:45 -0500

    badges added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
 // Check if html document is ready and loaded
 $(document).ready(function() {
 
+    //Snow Section
+    // snowStorm.snowColor = '#99ccff';   // blue-ish snow!?
+    // snowStorm.flakesMaxActive = 96;    // show more snow on screen at once
+    // snowStorm.useTwinkleEffect = true; 
+    // snowStorm.autoStart = true;
+
+    // $('#snowBtn').cllck()(function(){
+    //   snowStorm.toggleSnow();
+    // });
+
     // Message Submission Thank you Modal Trigger
     if(window.location.href.indexOf('#thankYou') != -1) {
       $('#thankYou').modal('show');
